ac6fc44703bed6991321ec70243bf060f89b56e1
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-07-07 11:36:51 -0400

    add scaffold files

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,22 @@ import { createRoot } from "react-dom/client";
 import React from 'react';
 const root = createRoot(document.getElementById("root"));
 
+import ProductDetail from './ProductDetail/ProductDetail.jsx'
+import QuestionsAnswers from './QuestionsAnswers/QuestionsAnswers.jsx'
+import RatingsReviews from './RatingsReviews/RatingsReviews.jsx'
+import RelatedItemsOutfits from './RelatedItemsOutfits/RelatedItemsOutfits.jsx'
+
 // Huzzah for jsx!
 const App = () => {
-  return <h1>Hello World</h1>
+
+  return (
+    <div>
+      <ProductDetail />
+      <QuestionsAnswers />
+      <RatingsReviews />
+      <RelatedItemsOutfits />
+    </div>
+  )
 }
 
 root.render(<App />);
