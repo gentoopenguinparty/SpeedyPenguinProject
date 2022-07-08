0868184a668f1e4f51036707d8f3d656b4ea32b6
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-07-08 19:38:01 -0400

    added search bar

--- src/QuestionsAnswers/QuestionsAnswers.jsx
+++ src/QuestionsAnswers/QuestionsAnswers.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {axiosGet, axiiosPost} from '../../util';
 
 export default function QuestionsAnswers() {
+  // const [data, setdata] = useState(null);
+  // const [error, setError] = useState("");
+  // const [loaded, setLoaded] = useState(false); //hr-rfe
 
-  return (<p>Questions and Answers</p>)
+    let param = 37315;
+    axiosGet(`https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rfe/qa/questions/?product_id=${param}`).then((data)=> {
+      console.log(data);
+    });
 
 
-}
+  return (
+
+  <div>
+     <p>Questions and Answers</p>
+
+     <input placeholder="Have A Question? Search For Answers..." />
+     <button type="submit">Search</button>
+     <br></br>
+
+     <textarea>
+
+     </textarea>
+  </div>
+)
+
+
+};
