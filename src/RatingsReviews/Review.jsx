151d971400c63d0642eb7596f57e91a2f8342c70
John Economou <johnlafayeti@gmail.com>
2022-07-10 16:52:12 -0400

    very basic review rendered with hooks

--- src/RatingsReviews/Review.jsx
+++ src/RatingsReviews/Review.jsx
@@ -4,7 +4,7 @@ import { axiosGet } from '../../util.js'
 
 export default function Review() {
 
-  const [stateData, modData] = useState(null);
+  const [stateData, modData] = useState([{ rating: 'filler' }]);
 
   useEffect(() => {
     axiosGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?product_id=37315')
@@ -15,6 +15,16 @@ export default function Review() {
   }, [])
 
   return (
-    <div>hi</div>
+    <div>
+      {stateData.map((review) => (
+        <div key={review.review_id}>
+          <div> {review.rating} </div>
+          <div> {review.reviewer_name} </div>
+          <div> {review.summary} </div>
+          <div> {review.body} </div>
+          <div> {review.recommend} </div>
+        </div>
+      ))}
+    </div>
   );
 }
