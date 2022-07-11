5ebfb21638c0ce21ac185f497dcc3029ece53a59
John Economou <johnlafayeti@gmail.com>
2022-07-11 16:58:24 -0400

    working on the tracker

--- src/RatingsReviews/RatingsReviews.jsx
+++ src/RatingsReviews/RatingsReviews.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BigList from './BigList.jsx';
-
+import SeeMore from './SeeMore.jsx';
 
 // Main component
 export default function RatingsReviews() {
+
+  const [trackCount, addTrack] = useState(0);
+
+    function tracker() {
+      console.log('clicked, count: ', addTrack(count + 1));
+    }
+
   return (
-    <BigList/>
+    <BigList>
+      <SeeMore tracker={tracker} trackCount={trackCount} addTrack={addTrack}>
+
+      </SeeMore>
+    </BigList>
   );
 
 }
