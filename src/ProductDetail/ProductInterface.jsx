5caff1fac0f282fd371b0a8971c12858deae10f1
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-12 09:03:10 -0400

    Added skeleton for Overview and laid out the product gallery.

---
+++ src/ProductDetail/ProductInterface.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Main = styled.div`
+background-color: white;
+color: #525252;
+padding:10px;
+`
+export default function ProductInterface(props) {
+console.log(props)
+
+  return (
+    <Main> Inter here </Main>
+  )
+}
