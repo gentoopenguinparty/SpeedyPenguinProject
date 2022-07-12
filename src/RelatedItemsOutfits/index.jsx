a2767231b855183c401b4f7cd9b0839672edcd65
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-07-12 15:14:22 -0400

    add getRelatedIDs method

--- src/RelatedItemsOutfits/index.jsx
+++ src/RelatedItemsOutfits/index.jsx
@@ -1,6 +1,17 @@
+/* eslint-disable no-console */
 import React from 'react';
+import { axiosGet } from '../../util.js';
+
 import RelatedProductCardCarousel from './RelatedProductCardCarousel.jsx';
 import OutfitCardCarousel from './OutfitCardCarousel.jsx';
+
+const relatedURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37313/related';
+
+function getRelatedIDs() {
+  axiosGet(relatedURL)
+    .then((productIDs) => productIDs)
+    .catch((err) => console.log(err));
+}
 
 export default function RelatedItemsOutfitsModule() {
   return (
