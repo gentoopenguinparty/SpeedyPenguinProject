e62e87391834af3697cd19dd356c2dac54741ade
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-07-12 16:17:13 -0400

    building out network requests

--- src/ProductDetail/ProductDetail.jsx
+++ src/ProductDetail/ProductDetail.jsx
@@ -4,9 +4,9 @@ import { axiosGet } from '../../util';
 
 export default function ProductDetail() {
 
-  axiosGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/meta/?product_id=37311')
-    .then((data) => console.log(data))
-    .catch((err) => console.log(err));
+  // axiosGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/meta/?product_id=37311')
+  //   .then((res) => console.log(res.data))
+  //   .catch((err) => console.log(err));
   const [searchParams, setSearchParams] = useSearchParams();
   const handleClick = () => {
     setSearchParams('123');
--- src/RelatedItemsOutfits/index.jsx
+++ src/RelatedItemsOutfits/index.jsx
@@ -1,19 +1,40 @@
 /* eslint-disable no-console */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { axiosGet } from '../../util.js';
 
 import RelatedProductCardCarousel from './RelatedProductCardCarousel.jsx';
 import OutfitCardCarousel from './OutfitCardCarousel.jsx';
 
-const relatedURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37313/related';
-
+// network request methods
 function getRelatedIDs() {
-  axiosGet(relatedURL)
-    .then((productIDs) => productIDs)
-    .catch((err) => console.log(err));
+  const relatedURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37313/related';
+  return axiosGet(relatedURL);
+}
+
+function getProductDetails(productID) {
+  const requestURL = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${productID}`;
+  return axiosGet(requestURL);
 }
 
 export default function RelatedItemsOutfitsModule() {
+  const [productInfo, setProductInfo] = useState({
+    category: '',
+    name: '',
+    price: '',
+    salePrice: '',
+    rating: 5,
+    images: [],
+    thumbnails: [],
+  });
+
+  useEffect(() => {
+    let promiseArray = getRelatedIDs()
+      .then((res) => res.data.map((id) => getProductDetails(id)))
+      .catch((err) => console.log(err));
+    console.log('promises:', promiseArray);
+    Promise.all(promiseArray).then((products) => console.log('products:', products));
+  });
+
   return (
     <div id="relatedProductsOutfitsModule">
       <RelatedProductCardCarousel />
