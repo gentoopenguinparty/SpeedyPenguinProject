34e00e512c6605480de2978c4be3375c6aab572a
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-12 12:45:34 -0400

    Added fullscreen functionality to gallery

--- src/ProductDetail/PhotoGallery.jsx
+++ src/ProductDetail/PhotoGallery.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+
 const Main = styled.div`
-background-image: url(${props => props.image});
+background-image: url(${(props) => props.image});
 background-color:#EBEBEB;
 background-repeat: no-repeat;
 background-size: contain ;
 background-position: center;
+width: ${(props) => props.size};
 color: white;
-`
+position:relative;
+z-index: 10;
+`;
 const Side = styled.div`
 display: flex;
 flex-direction: column;
 justify-content: space-around;
-`
+`;
 const Thumbnail = styled.div`
-background-image: url(${props => props.image});
+background-image: url(${(props) => props.image});
 background-color: white;
 background-repeat: no-repeat;
 background-size: contain ;
@@ -23,14 +27,27 @@ height: 50px;
 width: 50px;
 margin:5px;
 border: 0.5px solid black;
-`
-export default function PhotoGallery(props) {
-  console.log( props.images.photos[1])
+`;
+const FullScreen = styled.div`
+width: 20px;
+height: 20px;
+background-color: red;
+position: absolute;
+right: 10px;
+top:10px;
+`;
+export default function PhotoGallery({ images }) {
+  const [isFullScreen, setIsFullScreen] = React.useState(false)
+
   return (
-    <Main image ={props.images.photos[4].url}>
+    <Main
+      size={isFullScreen ? '100vw' : 'auto'}
+      image={images.photos[4].url}
+    >
       <Side>
-      {props.images.photos.map((photo, i) => <Thumbnail key ={i} image= {photo.thumbnail_url}/>)}
+        {images.photos.map((photo, i) => <Thumbnail key={i} image={photo.thumbnail_url} />)}
       </Side>
+      <FullScreen onClick={() => setIsFullScreen((prev) => !prev)} />
     </Main>
-  )
+  );
 }
--- src/ProductDetail/ProductDetail.jsx
+++ src/ProductDetail/ProductDetail.jsx
@@ -1,40 +1,51 @@
 import React from 'react';
 import { useSearchParams } from 'react-router-dom';
+import styled from 'styled-components';
 import { axiosGet } from '../../util';
-import styled from 'styled-components'
-import PhotoGallery from './PhotoGallery.jsx'
-import ProductDescription from './ProductDescription.jsx'
-import ProductInterface from './ProductInterface.jsx'
-import ProductCheckmarks from './ProductCheckmarks.jsx'
-const Grid = styled.div`
+import PhotoGallery from './PhotoGallery.jsx';
+import ProductDescription from './ProductDescription.jsx';
+import ProductInterface from './ProductInterface.jsx';
+import ProductCheckmarks from './ProductCheckmarks.jsx';
+
+const Grid1 = styled.div`
 display:grid;
 grid-template: 2fr 1fr / 1.5fr 1fr;
-`
+`;
+const Grid2 = styled.div`
+display:grid;
+grid-template: 2fr 1fr / 1.5fr 1fr;
+`;
 
 export default function ProductDetail() {
-
-const [productData, setProductData] = React.useState(null)
-const [styles, setStyles] = React.useState([])
-const [currentStyle, setCurrrentStyle] = React.useState(0)
-React.useEffect (() => {
-  axiosGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37311')
-    .then((data) =>{ setProductData(data.data)})
-    .catch((err) => console.log(err));
-  axiosGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37311/styles')
-    .then((data) => setStyles(data.data.results))
-    .catch((err) => console.log(err));
-
-
-},[])
+  const [productData, setProductData] = React.useState(null);
+  const [styles, setStyles] = React.useState([]);
+  const [currentStyle, setCurrrentStyle] = React.useState(0);
+  React.useEffect(() => {
+    axiosGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37311')
+      .then((data) => {
+        setProductData(data.data);
+        axiosGet('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/37311/styles')
+          .then((data) => setStyles(data.data.results))
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
-    <>{!styles.length ? <div>Loading</div>: (
-      <Grid>
-        <PhotoGallery images ={styles[currentStyle]} />
-        <ProductInterface data ={productData} styles ={styles} />
-        <ProductDescription />
-        <ProductCheckmarks />
-      </Grid>
-    )}</>
+    <div>
+      {!styles.length ? <div>Loading</div> : (
+        <>
+          <Grid1>
+            <PhotoGallery images={styles[currentStyle]} />
+            <ProductInterface data={productData} styles={styles} />
+          </Grid1>
+
+          <Grid2>
+            <ProductDescription />
+            <ProductCheckmarks />
+          </Grid2>
+        </>
+      )}
+    </div>
   );
 }
