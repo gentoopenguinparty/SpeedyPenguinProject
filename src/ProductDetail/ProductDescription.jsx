d3a43ff54e187487504f3ce00aa740585500cb3a
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-12 20:37:15 -0400

    Added Desc Skel

---
+++ src/ProductDetail/ProductDescription.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styled from 'styled-components'
+const Main = styled.div`
+background-color: green;
+color: white;
+`
+export default function ProductDescription(props) {
+
+
+  return (
+    <Main> Desc here </Main>
+  )
+}
