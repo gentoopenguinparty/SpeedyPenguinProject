352b91535f97b7a682f8076f3d327b4b6588e080
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-07-12 21:53:53 -0400

    zustand

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-<<<<<<< HEAD
 import create from 'zustand';
-=======
->>>>>>> dev
 
 import ProductDetail from './ProductDetail/ProductDetail.jsx';
 import QuestionsAnswers from './QuestionsAnswers/QuestionsAnswers.jsx';
@@ -13,7 +10,6 @@ import RelatedItemsOutfits from './RelatedItemsOutfits/index.jsx';
 
 const root = createRoot(document.getElementById('root'));
 
-<<<<<<< HEAD
 // creates Zustand store with valid initial ID
 // maybe should be in a util file?
 const useIDStore = create((set) => ({
@@ -27,9 +23,6 @@ export default useIDStore;
 // REACT-ROUTER:
 // on page load invoke changeID with id from URL
 
-=======
-// Huzzah for jsx!
->>>>>>> dev
 function App() {
   return (
     <div>
