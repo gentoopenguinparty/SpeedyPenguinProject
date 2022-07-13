c6d1d8aff7ce3a55692489e9fc3b16595f7f96f3
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-07-12 21:50:53 -0400

    merge Zustand branch (#11)
    * create simple Zustand store for productID
    * add comments explaining Zustand usage

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
+<<<<<<< HEAD
+import create from 'zustand';
+=======
+>>>>>>> dev
 
 import ProductDetail from './ProductDetail/ProductDetail.jsx';
 import QuestionsAnswers from './QuestionsAnswers/QuestionsAnswers.jsx';
@@ -9,7 +13,23 @@ import RelatedItemsOutfits from './RelatedItemsOutfits/index.jsx';
 
 const root = createRoot(document.getElementById('root'));
 
+<<<<<<< HEAD
+// creates Zustand store with valid initial ID
+// maybe should be in a util file?
+const useIDStore = create((set) => ({
+  currentProductID: 37313,
+  changeID: (newID) => set({ currentProductID: newID }),
+}));
+export default useIDStore;
+// inside components that need to know ID:
+// const currentProductID = useIDStore((state) => state.currentProductID)
+
+// REACT-ROUTER:
+// on page load invoke changeID with id from URL
+
+=======
 // Huzzah for jsx!
+>>>>>>> dev
 function App() {
   return (
     <div>
