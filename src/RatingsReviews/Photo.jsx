f17b0b0968321870b467e829122f5c8921e7099b
John Economou <johnlafayeti@gmail.com>
2022-07-13 19:50:34 -0400

    can add a single photo to the review

--- src/RatingsReviews/Photo.jsx
+++ src/RatingsReviews/Photo.jsx
@@ -2,9 +2,18 @@ import React, { useState } from 'react'
 
 export default function Photo() {
 
+  const [file, setFile] = useState('');
+
+  function handleFile(event) {
+    console.log(event.target.files[0]);
+    setFile(event.target.files[0]);
+  }
+
   return (
     <div>
-      PhotoTime!
+      <input type='file' onChange={handleFile} />
+      {file.length !== 0 ? <img width='100' height='100'src={URL.createObjectURL(file)} /> :
+        null}
     </div>
   )
 }
