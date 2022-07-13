c929ddc77f9f819a4c786e872ade29ae63f2a274
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-13 12:35:24 -0400

    Style selector looks fine

--- src/ProductDetail/ProductInterface.jsx
+++ src/ProductDetail/ProductInterface.jsx
@@ -11,12 +11,72 @@ grid-row: 1/2;
 z-index:1;
 border: 2px solid blue;
 `;
-export default function ProductInterface({ data, styles }) {
-  console.log();
+const Rating = styled.div`
+--star-size: 60px;
+--star-color: #fff;
+--star-background: #fc0;
 
+&:before {
+  content: "★★★★★";
+  letter-spacing: 3px;
+  background: linear-gradient(90deg, var(--star-background) calc(${(props) => props.rating} / 5 * 100%), var(--star-color) calc(${(props) => props.rating} / 5 * 100%));
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}`;
+const Category = styled.p`
+`;
+const Name = styled.h2`
+
+`;
+const Price = styled.p`
+
+`;
+const StyleGrid = styled.div`
+height: 50%;
+display: grid;
+grid-template-columns: 1fr 1fr 1fr 1fr;
+justify-items: center;
+align-items: center;
+`;
+
+const StyleThumbnail = styled.div`
+
+width: 60px;
+height: 60px;
+border-radius: 50%;
+background-color: grey;
+background-image: url(${(props) => props.image});
+background-size: contain;
+`;
+const StyleCheck = styled.div`
+width:15px;
+height: 15px;
+border: 1px solid black;
+border-radius: 50%;
+float: right;
+background-color: white;
+background-image: url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjj2DOSD1yUrkY0TbTbWhy0okIcvxHGhfP2bxtbDhkNQ4r1NLLGbKGzgLmIeqcM3arMHM&usqp=CAU);
+background-size:contain;
+`;
+
+export default function ProductInterface({ data, styles }) {
+  console.log(styles);
+  const currentId = 0;
   return (
     <Main>
-      {data.name}
+      <Rating rating={5} />
+      <Category>Filler</Category>
+      <Name>{data.name}</Name>
+      <Price>$123</Price>
+      <StyleGrid>
+        {
+        styles.map((style, i) => (
+          <StyleThumbnail key={style.style_id} image={style.photos[0].thumbnail_url}>
+            {currentId === i ? <StyleCheck /> : ''}
+          </StyleThumbnail>
+        ))
+          }
+      </StyleGrid>
     </Main>
   );
 }
