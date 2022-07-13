428f0ddea0d448d7a8483e5d58ef13e880b05e34
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-13 10:01:18 -0400

    Es Linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,12 +18,6 @@ module.exports = {
     'react',
   ],
   rules: {
-<<<<<<< HEAD
-    "import/extensions": [ "never"],
-    'import/prefer-default-export': 'off',
-    'eol-last': 'off',
-=======
     'import/extensions': 0,
->>>>>>> c11d293c1e7cbbfa79c70515e2163ede972f6f87
   },
 };
