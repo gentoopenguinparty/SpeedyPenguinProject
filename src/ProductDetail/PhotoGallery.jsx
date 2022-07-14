2a65f5ff6fdc224d8d52c3b67b362205043c8d31
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-14 16:45:57 -0400

    Added FullScreen and Zoomed functionality

--- src/ProductDetail/PhotoGallery.jsx
+++ src/ProductDetail/PhotoGallery.jsx
@@ -1,6 +1,99 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+export default function PhotoGallery({ images }) {
+  const [currentPicInd, setCurrentPicInd] = useState(0);// CHANGE TO PARENT LATER
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [zoomed, setZoomed] = useState(false);
+  const [imagePos, setImagePos] = useState('center');
+  const [sideBarOffset, setSideBarOffset] = useState(0);
+  const handleImageMove = (e) => {
+    setImagePos(`-${e.nativeEvent.offsetX * 2.3}px -${e.nativeEvent.offsetY * 4}px`);
+  };
+  const handleFullScreen = (e) => {
+    if (e.target.id === 'current-image') {
+      setIsFullScreen(true);
+    }
+  };
+  return (
+    <Main
+      className="test"
+      size={isFullScreen ? '100vw' : 'auto'}
+    >
+      <CurrentImage
+        id="current-image"
+        pos={imagePos}
+        image={images.photos[currentPicInd].url}
+        onClick={isFullScreen ? () => setZoomed((prev) => !prev) : handleFullScreen}
+        fullScreen={isFullScreen}
+        zoomed={zoomed}
+        onMouseMove={zoomed ? handleImageMove : () => ''}
+      >
+        { !isFullScreen ? (
+          <Side>
+            {sideBarOffset !== 0
+              ? (
+                <SideBarArrow
+                  onClick={() => setSideBarOffset((prev) => prev - 1)}
+                  direction={-90}
+                />
+              ) : ''}
+            {images.photos.map((photo, i) => (
+              <Thumbnail
+                onClick={() => setCurrentPicInd(i)}
+                key={i}
+                image={photo.thumbnail_url}
+                className={currentPicInd === i ? 'border-b-4 border-indigo-500' : ''}
+              />
+            )).slice(sideBarOffset, sideBarOffset + 7)}
+            {!(sideBarOffset + 7 === images.photos.length)
+              ? (
+                <SideBarArrow
+                  onClick={() => setSideBarOffset((prev) => prev + 1)}
+                  direction={90}
+                />
+              ) : ''}
+          </Side>
+        ) : ''}
+        {isFullScreen ? (
+          <FullScreen onClick={() => {
+            setZoomed(true);
+            setIsFullScreen((prev) => !prev);
+          }}
+          />
+        ) : ''}
+        {!(currentPicInd === 0)
+          ? (
+            <Arrow
+              onClick={() => {
+                if (currentPicInd <= sideBarOffset) {
+                  setSideBarOffset((prev) => prev - 1);
+                }
+                setCurrentPicInd((prev) => prev - 1);
+              }}
+              left="80px"
+              flip={-1}
+            />
+          ) : ''}
+
+        {!((currentPicInd + 1) === images.photos.length)
+          ? (
+            <Arrow
+              onClick={() => {
+                if (currentPicInd >= sideBarOffset + 6) {
+                  setSideBarOffset((prev) => prev + 1);
+                }
+                setCurrentPicInd((prev) => prev + 1);
+              }}
+              right="25px"
+              flip={1}
+            />
+          ) : ''}
+
+      </CurrentImage>
+    </Main>
+  );
+}
 const Main = styled.div`
 width: ${(props) => props.size};
 color: white;
@@ -9,15 +102,24 @@ background-color:none;
 `;
 const CurrentImage = styled.div.attrs((props) => ({
   style: {
-    backgroundPosition: props.pos,
+    backgroundPosition: props.zoomed ? props.pos : 'center',
   },
 }))`
+  min-height: 460px;
   background-image: url(${(props) => props.image});
   background-color:grey;
   background-repeat: no-repeat;
-  background-size: cover ;
-  transition: all 1s;
+  background-size: ${(props) => (!props.zoomed ? 'cover' : '250%')} ;
+  transition: all 0s;
   width:100%;
+  cursor: ${(props) => {
+    if (!props.fullScreen) {
+      return 'zoom-in';
+    } if (!props.zoomed) {
+      return 'crosshair';
+    }
+    return 'zoom-out';
+  }};
   `;
 
 const Side = styled.div`
@@ -29,14 +131,15 @@ width: 80px;
 `;
 const Thumbnail = styled.div`
 background-image: url(${(props) => props.image});
-background-color: white;
+background-color: rgba(255,255,255,0.5);
 background-repeat: no-repeat;
 background-size: contain ;
 background-position: center;
 height: 50px;
 width: 50px;
 margin:5px;
-border: 0.5px solid black;
+border: 0.2px solid rgba(0,0,0,0.5);
+cursor: pointer;
 `;
 const FullScreen = styled.div`
 width: 20px;
@@ -44,91 +147,35 @@ height: 20px;
 background-image: url(https://cdn0.iconfinder.com/data/icons/bold-multimedia-player-button/512/media_player_button_24-512.png);
 background-size: contain;
 position: absolute;
-right: 10px;
+right: 40px;
 top:10px;
 cursor: pointer;
 `;
 const Arrow = styled.div`
 position: absolute;
+background-size:contain;
+background-position: center;
+background-repeat: no-repeat;
 background-image: url(https://freepngimg.com/thumb/arrow/5-2-arrow-transparent.png);
+background-color:rgba(255,255,255,0.5);
+border-radius:30%;
 right: ${(props) => props.right};
 left: ${(props) => props.left};
 bottom:50%;
 height:20px;
 width:20px;
-background-size:contain;
-background-position: center;
-background-repeat: no-repeat;
 transform: scaleX(${(props) => props.flip});
 cursor: pointer;
 `;
 const SideBarArrow = styled.div`
 height:20px;
 width:20px;
-background-color:red;
+background-size:contain;
+background-position: center;
+background-repeat: no-repeat;
+background-image: url(https://freepngimg.com/thumb/arrow/5-2-arrow-transparent.png);
+background-color:rgba(255,255,255,0.5);
+border-radius:30%;
+transform:rotate(${(props) => props.direction}deg);
+cursor: pointer;
 `;
-export default function PhotoGallery({ images }) {
-  const [currentPicInd, setCurrentPicInd] = useState(0);// CHANGE TO PARENT LATER
-  const [isFullScreen, setIsFullScreen] = useState(false);
-  const [imagePos, setImagePos] = useState('center');
-  const [sideBarOffset, setSideBarOffset] = useState(0);
-  const handleImageMove = (e) => {
-    setImagePos(`center -${e.nativeEvent.offsetY * (isFullScreen ? 3 : 1)}px`);
-  };
-
-  return (
-    <Main
-      className="test"
-      size={isFullScreen ? '100vw' : 'auto'}
-    >
-      <CurrentImage
-        pos={imagePos}
-        image={images.photos[currentPicInd].url}
-      >
-        <Side>
-          {sideBarOffset !== 0
-            ? (
-              <SideBarArrow
-                onClick={() => setSideBarOffset((prev) => prev - 1)}
-                flip={1}
-              />
-            ) : ''}
-          {images.photos.map((photo, i) => (
-            <Thumbnail
-              onClick={() => setCurrentPicInd(i)}
-              key={i}
-              image={photo.thumbnail_url}
-            />
-          )).slice(sideBarOffset, sideBarOffset + 7)}
-          {!(sideBarOffset + 7 === images.photos.length)
-            ? (
-              <SideBarArrow
-                onClick={() => setSideBarOffset((prev) => prev + 1)}
-                flip={1}
-              />
-            ) : ''}
-        </Side>
-        <FullScreen onClick={() => setIsFullScreen((prev) => !prev)} />
-
-        {!((currentPicInd + 1) === images.photos.length)
-          ? (
-            <Arrow
-              onClick={() => setCurrentPicInd((prev) => prev + 1)}
-              right="10px"
-              flip={1}
-            />
-          ) : ''}
-
-        {!(currentPicInd === 0)
-          ? (
-            <Arrow
-              onClick={() => setCurrentPicInd((prev) => prev - 1)}
-              left="80px"
-              flip={-1}
-            />
-          ) : ''}
-
-      </CurrentImage>
-    </Main>
-  );
-}
