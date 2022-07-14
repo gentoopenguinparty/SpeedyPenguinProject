a04b62c5f2ab3278d9665e905ebf5aa21255ffaf
John Economou <johnlafayeti@gmail.com>
2022-07-14 19:02:51 -0400

    helpful filter now works

--- src/RatingsReviews/DropDown.jsx
+++ src/RatingsReviews/DropDown.jsx
@@ -9,6 +9,37 @@ export default function DropDown({ modData, apiData }) {
   const [active, setActive] = useState(false);
   const [filter, setFilter] = useState('relevence')
 
+  var sortHelpful = function (array) {
+    // set a counter = 0
+    var count = 0;
+    console.log('array', array)
+    console.log('inside Function help', array[0].helpfulness);
+    // Create a for loop that goes through the entire array
+    for (var i = 0; i < array.length - 1; i++) {
+      console.log('for', array[i + 1].helpfulness, array[i].helpfulness)
+      if (array[i + 1].helpfulness > array[i].helpfulness) {
+        // swap the values around
+        var currentVal = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = currentVal;
+        // increment a counter
+        count++;
+      }
+    }
+    // if counter = 0
+    if (count === 0) {
+      // then return the sorted array
+      modData([...array]);
+      console.log('help')
+      return;
+    }
+    // if counter > 0
+    if (count > 0) {
+      // run sortNew again with the current array
+      sortHelpful(array);
+    }
+    return;
+  }
 
   var sortNew = function (array) {
     // set a counter = 0
@@ -74,7 +105,7 @@ export default function DropDown({ modData, apiData }) {
           <div onClick={() => { setFilter('newest'); sortNew(apiData) }}>
             newest
           </div>
-          <div onClick={() => { setFilter('helpful') }}>
+          <div onClick={() => { setFilter('helpful'); sortHelpful(apiData)}}>
             helpful
           </div>
         </DropList>
--- src/RatingsReviews/blah.js
+++ src/RatingsReviews/blah.js
@@ -1,34 +1,34 @@
-var sortNew = function(array) {
+var sortHelpful = function (array) {
   // set a counter = 0
   var count = 0;
-  console.log('inside Function', array[0].date);
+  console.log('array', array)
+  console.log('inside Function help', array[0].helpfulness);
   // Create a for loop that goes through the entire array
   for (var i = 0; i < array.length - 1; i++) {
-    // console.log('for', array[i + 1].date.slice(0,4), array[i].date.slice(0,4) )
-    console.log(JSON.stringify(array), i)
-    if (parseInt(array[i + 1].date.slice(0,4)) > parseInt(array[i].date.slice(0,4))) {
+    console.log('for', array[i + 1].helpfulness, array[i].helpfulness)
+    if (array[i + 1].helpfulness > array[i].helpfulness) {
       // swap the values around
       var currentVal = array[i];
       array[i] = array[i + 1];
       array[i + 1] = currentVal;
-       // increment a counter
+      // increment a counter
       count++;
     }
   }
   // if counter = 0
   if (count === 0) {
     // then return the sorted array
-    console.log(array);
+    console.log(JSON.stringify(array))
     return;
   }
   // if counter > 0
   if (count > 0) {
     // run sortNew again with the current array
-    sortNew(array);
-}
-return;
+    sortHelpful(array);
+  }
+  return;
 }
 
-data=[{date: '2022-01-03T00:00:00.000Z'} ,{date: "2022-01-03T00:00:00.000Z"},{date: "2021-11-09T00:00:00.000Z"},{date: "2022-01-07T00:00:00.000Z"},{date: "2022-05-30T00:00:00.000Z"}]
+data=[{helpfulness: 32} ,{helpfulness: 8},{helpfulness: 28},{helpfulness: 1},{helpfulness: 3}]
 
-sortNew(data);
+sortHelpful(data);
