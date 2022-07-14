97b68a3d8a894ae123c587fdd5c9953902cccc65
John Economou <johnlafayeti@gmail.com>
2022-07-14 09:54:19 -0400

    fixed the bug with the star rating

--- src/RatingsReviews/styles/Rating.styled.js
+++ src/RatingsReviews/styles/Rating.styled.js
@@ -2,14 +2,23 @@ import styled from 'styled-components';
 
 export const Rating = styled.div
 `
+--rating: ${props=>props.rating};
+--percent: calc(var(--rating) / 5 * 100%);
+
 --star-size: 60px;
 --star-color: #fff;
 --star-background: #000000;
 -webkit-text-stroke: 1px black;
+
+display: inline-block;
+font-size: var(--star-size);
+font-family: Times;
+line-height: 1;
+
 &:before {
-  content: "★★★★★";
+  content: '★★★★★';
   letter-spacing: 3px;
-  background: linear-gradient(90deg, var(--star-background) calc((${props => props.rating})/ 5 * 100%), var(--star-color) calc(${props => props.rating} / 5 * 100%));
+  background: linear-gradient(90deg, var(--star-background) var(--percent), var(--star-color) var(--percent));
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }`
