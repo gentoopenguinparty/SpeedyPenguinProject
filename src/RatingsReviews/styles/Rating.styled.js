f905a4b97de452bdcb0a5b7b4005760b4a5ffa59
John Economou <johnlafayeti@gmail.com>
2022-07-14 09:42:44 -0400

    star ratings are not working entirely as intended will have to fix in future

--- src/RatingsReviews/styles/Rating.styled.js
+++ src/RatingsReviews/styles/Rating.styled.js
@@ -4,12 +4,12 @@ export const Rating = styled.div
 `
 --star-size: 60px;
 --star-color: #fff;
---star-background: #fc0;
+--star-background: #000000;
 -webkit-text-stroke: 1px black;
 &:before {
   content: "★★★★★";
   letter-spacing: 3px;
-  background: linear-gradient(90deg, var(--star-background) calc(${props => props.rating} / 5 * 100%), var(--star-color) calc(${props => props.rating} / 5 * 100%));
+  background: linear-gradient(90deg, var(--star-background) calc((${props => props.rating})/ 5 * 100%), var(--star-color) calc(${props => props.rating} / 5 * 100%));
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
 }`
