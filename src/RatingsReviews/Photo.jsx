d3a1d6948ecd853dcf76573f12edb7530ef2da16
John Economou <johnlafayeti@gmail.com>
2022-07-13 20:32:16 -0400

    can now add multiple thumbnails

--- src/RatingsReviews/Photo.jsx
+++ src/RatingsReviews/Photo.jsx
@@ -2,18 +2,22 @@ import React, { useState } from 'react'
 
 export default function Photo() {
 
-  const [file, setFile] = useState('');
+  const [files, setFile] = useState('');
 
   function handleFile(event) {
     console.log(event.target.files[0]);
-    setFile(event.target.files[0]);
+    let concatFile = [...files, event.target.files[0]];
+    setFile(concatFile);
   }
 
   return (
     <div>
       <input type='file' onChange={handleFile} />
-      {file.length !== 0 ? <img width='100' height='100'src={URL.createObjectURL(file)} /> :
-        null}
+      {files.length !== 0 ? (files.map((file) => {
+        return (
+          <img width='100' height='100' src={URL.createObjectURL(file)} />
+        )
+      })) : null}
     </div>
   )
 }
