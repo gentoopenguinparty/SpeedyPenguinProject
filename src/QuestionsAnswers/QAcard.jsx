f7b06c79c01fa588c788906fbf61ce5eb95f9cd6
Cheyenne Cornett <cheyenne.cornett22@gmail.com>
2022-07-15 12:18:44 -0400

    Load more Qs  only adds 2 more at a time. Button hides if no more Qs

--- src/QuestionsAnswers/QAcard.jsx
+++ src/QuestionsAnswers/QAcard.jsx
@@ -1,6 +1,8 @@
 
 import AnswerCard from './AnswerCards.jsx';
 import React, { useEffect, useState } from 'react';
+import {Container} from './styles/Main.styled.js'
+// import {qListLength, setQListLength} from '../QuestionsAnswers/QuestionsAnswers.jsx'
 
 export default function QAcard(props) {
 
@@ -8,44 +10,17 @@ export default function QAcard(props) {
 
 
   const displayQAcards = (props) => {
-
-    if (props.loadMoreQs === true) {
-      const { qaCards } = props;
-      if (qaCards.length > 0) {
-
-      return (
-        qaCards.filter(qaCard => qaCard.question_body.toLowerCase().includes(props.search.toLowerCase())).sort((a, b) => a.question_helpfulness > b.question_helpfulness ? -1 : 1).map((qaCard, index) => {
-
-          return (
-
-           <div className="QAcard" key={qaCard.question_id}>
-              <h3>
-                Q:
-                {' '}
-                {qaCard.question_body}
-              </h3>
-              <small>
-                <pre>
-                  Helpful? <span onClick={() => { console.log('yes question test') }}>Yes(
-                    {qaCard.question_helpfulness}
-                    ) |</span>  <span onClick={() => { console.log('Add answer test') }}>Add Answer</span>
-                </pre>
-              </small>
-              <AnswerCard answers={qaCard.answers}/>
-            </div>
+    console.log(props)
 
 
-          );
-        })
 
-      );
-    }
-  } else {
+
     const { qaCards } = props;
     if (qaCards.length > 0) {
 
+
       return (
-        qaCards.slice(0, 4).filter(qaCard => qaCard.question_body.toLowerCase().includes(props.search.toLowerCase())).sort((a, b) => a.question_helpfulness > b.question_helpfulness ? -1 : 1).map((qaCard, index) => {
+        qaCards.slice(0, props.qListLength ).filter(qaCard => qaCard.question_body.toLowerCase().includes(props.search.toLowerCase())).sort((a, b) => a.question_helpfulness > b.question_helpfulness ? -1 : 1).map((qaCard, index) => {
 
           return (
             <div className="QAcard" key={qaCard.question_id}>
@@ -68,9 +43,13 @@ export default function QAcard(props) {
 
       );
     }
-  }
+
     return (
-      <p>No Questions Asked At This Time</p>
+      <>
+      <h3>No Questions Asked At This Time</h3>
+      <button onClick={() => { console.log('Add answer test') }}>Add Answer</button>
+      </>
+
     );
   };
   return (
--- src/QuestionsAnswers/QuestionsAnswers.jsx
+++ src/QuestionsAnswers/QuestionsAnswers.jsx
@@ -11,25 +11,39 @@ import {Container} from './styles/Main.styled.js'
 export default function QuestionsAnswers() {
   const [data, setData] = useState('');
   const [search, setSearch] = useState(``)
-  const [loadMoreQs, setLoadMoreQs] = useState(false);
+  const [noMoreQs, setNoMoreQs] = useState(false);
+  const [qListLength, setQListLength] = useState(4)
 
   //const [dataLength, setDataLength] = useState()
   // const [error, setError] = useState("");
   // const [loaded, setLoaded] = useState(false); //hr-rfe
- console.log(search);
+
   useEffect(() => {
     getQAs();
   }, []);
 
-  const param = 37313;
+
+
+  const incrementQList = () => {
+    console.log(data.length);
+    if (qListLength >= data.length -2) {
+      setNoMoreQs(true);
+    }
+
+    setQListLength(qListLength + 2)
+
+  }
+
+
+  const param = 37325;
 
   const getQAs = () => axiosGet(`https://app-hrsei-api.herokuapp.com/api/fec2/:hr-rfe/qa/questions/?product_id=${param}`).then((response) => {
     // console.log(response.data.results);
     const allData = response.data.results;
     setData(allData);
-  }).catch((error) => { console.error(`Error, ${error}`); });
+  }).then((data) =>{if (data.length <= 4) {setNoMoreQs(true)}}).catch((error) => { console.error(`Error, ${error}`); });
 
-  console.log('data:', data);
+  console.log('data:', data.length);
   // only four questions with two answers per question will be rendered  onChange={e => setSearch(e.target.value)}
   //
 
@@ -42,12 +56,12 @@ export default function QuestionsAnswers() {
 
       <br/>
 
-      <QAcard qaCards={data} search={search} loadMoreQs={loadMoreQs} />
+      <QAcard qaCards={data} search={search} noMoreQs={noMoreQs} qListLength={qListLength} />
       <br></br>
       <br></br>
       <>
       {/* <button onClick={() =>{setLoadMoreQs(true)}}>Load More Questions</button> */}
-      {!loadMoreQs || data.length < 4 && <button  onClick={() =>{setLoadMoreQs(true)}}>Load More Questions</button>}
+      {!noMoreQs && <button  onClick={incrementQList}>Load More Questions</button>}
       </>
 
     </Container>
