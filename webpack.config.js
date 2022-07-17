6b819fa9743791cdec8e3bb8f233132c0079e2c9
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-07-17 17:17:58 -0400

    add webpack config rule to process local images

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,34 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/index.jsx",
+  mode: 'development',
+  entry: './src/index.jsx',
   output: {
     path: path.join(__dirname, 'public'),
-    filename: "bundle.js"
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(jsx|js)$/,
         exclude: /node_modules/,
-        loader: "babel-loader"
+        loader: 'babel-loader',
       },
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
-  // [devtool] this is an additional source map that will let the browser know what files are running our code.
-  // Helps with error tracing. Without it we will not know where our errors are coming from because it will state that everything inside the bundle file.
-  devtool: "eval-cheap-module-source-map",
+  // [devtool] this is an additional source map that will let the browser
+  // know what files are running our code. Helps with error tracing.
+  // Without it we will not know where our errors are coming from
+  // because it will state that everything inside the bundle file.
+  devtool: 'eval-cheap-module-source-map',
   // [devServer] configuration for the live server including port
   devServer: {
     // [static] config for how what to serve
@@ -32,5 +38,5 @@ module.exports = {
     compress: true,
     // [port] what port on our local machine to run the dev server
     port: 3000,
-  }
-}
+  },
+};
