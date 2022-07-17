5ed546e13af0a32c62a58f9b86b148773e014c19
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-07-17 19:55:36 -0400

    tweak flex settings in CardCarousel styled component

--- src/RelatedItemsOutfits/styles/CardCarousel.styled.js
+++ src/RelatedItemsOutfits/styles/CardCarousel.styled.js
@@ -2,14 +2,14 @@ import styled from 'styled-components';
 
 export const FlexContainer = styled.div`
   display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const CardCarousel = styled.div`
   display: flex;
   justify-content: space-around;
   align-items: center;
-
-  margin-right: auto;
 
   height: 400px;
   padding: 10px;
