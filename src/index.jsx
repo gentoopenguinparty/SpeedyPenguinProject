c5cf9efd5f8ffa577b0ab44de07207b7cd4afd65
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-18 15:21:52 -0400

    Added Central API call and url parser (#21)
    * Central API call
    
    * Added link functionality
    
    * PLease dont duplicate
    
    * fix typo
    
    Co-authored-by: Anthony McGovern <mcgovern.anthony@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,34 +1,55 @@
 import { createRoot } from 'react-dom/client';
-import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
 import create from 'zustand';
 import ProductDetail from './ProductDetail/ProductDetail.jsx';
 import QuestionsAnswers from './QuestionsAnswers/QuestionsAnswers.jsx';
 import RatingsReviews from './RatingsReviews/RatingsReviews.jsx';
 import RelatedItemsOutfitsModule from './RelatedItemsOutfits/index.jsx';
+import { getAll } from '../util.js';
 
 const root = createRoot(document.getElementById('root'));
 
 // creates Zustand store with valid initial ID
 // maybe should be in a util file?
+
 const useIDStore = create((set) => ({
   currentProductID: 37313,
   changeID: (newID) => set({ currentProductID: newID }),
 }));
 export default useIDStore;
-// inside components that need to know ID:
+
+// inside components tht need to know ID:
 // const currentProductID = useIDStore((state) => state.currentProductID)
 
 // REACT-ROUTER:
 // on page load invoke changeID with id from URL
 
 function App() {
+  const url = window.location.href;
+  const id = +url.slice(url.length-6, url.length-1) || 38000;
+  const [currentProductData, setCurrentProductData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    getAll(id)
+      .then((responses) => setCurrentProductData(responses.map((response) => response.data)))
+      .then(() => setLoaded(true));
+  }, []);
   return (
+
     <div>
-      <ProductDetail />
-      <RelatedItemsOutfitsModule />
-      <QuestionsAnswers />
-      <RatingsReviews />
+      {loaded ? (
+        <div>
+
+          <ProductDetail
+            productData={currentProductData[0]}
+            styles={currentProductData[1].results}
+          />
+          <RelatedItemsOutfitsModule />
+          <QuestionsAnswers />
+          <RatingsReviews />
+        </div>
+      ) : <p>Loading</p> }
     </div>
   );
 }
