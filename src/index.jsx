3d0aa96d7ef1e4481c20e29a7743ee82c6b404a8
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-18 14:49:50 -0400

    Central API call

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,18 @@
 import { createRoot } from 'react-dom/client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import create from 'zustand';
 import ProductDetail from './ProductDetail/ProductDetail.jsx';
 import QuestionsAnswers from './QuestionsAnswers/QuestionsAnswers.jsx';
 import RatingsReviews from './RatingsReviews/RatingsReviews.jsx';
 import RelatedItemsOutfitsModule from './RelatedItemsOutfits/index.jsx';
+import { getAll } from '../util.js';
 
 const root = createRoot(document.getElementById('root'));
 
 // creates Zustand store with valid initial ID
 // maybe should be in a util file?
+
 const useIDStore = create((set) => ({
   currentProductID: 37313,
   changeID: (newID) => set({ currentProductID: newID }),
@@ -23,12 +25,26 @@ export default useIDStore;
 // on page load invoke changeID with id from URL
 
 function App() {
+  const [currentProductData, setCurrentProductData] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  useEffect(() => {
+    getAll(37311)
+      .then((responses) => setCurrentProductData(responses.map((response) => response.data)))
+      .then(() => setLoaded(true));
+  }, []);
   return (
     <div>
-      <ProductDetail />
-      <RelatedItemsOutfitsModule />
-      <QuestionsAnswers />
-      <RatingsReviews />
+      {loaded ? (
+        <div>
+          <ProductDetail
+            productData={currentProductData[0]}
+            styles={currentProductData[1].results}
+          />
+          <RelatedItemsOutfitsModule />
+          <QuestionsAnswers />
+          <RatingsReviews />
+        </div>
+      ) : <p>Loading</p> }
     </div>
   );
 }
--- util.js
+++ util.js
@@ -19,8 +19,15 @@ export function axiosPost(url, data) {
 export function axiosPut(url) {
   return axios.put(url, null, {
     headers: {
-      'Authorization': API_KEY,
-    }
+      Authorization: API_KEY,
+    },
   });
 }
 
+export function getAll(id) {
+  const array = [axiosGet(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}`),
+    axiosGet(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/${id}/styles`),
+    axiosGet(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/meta/?product_id=${id}`),
+    axiosGet(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?product_id=${id}`)];
+  return Promise.all(array);
+}
