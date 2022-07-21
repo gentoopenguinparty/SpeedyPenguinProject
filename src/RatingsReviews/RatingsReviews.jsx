0b9a98f3ceaa1b4dd428b9c1aa2a3efb47a74315
John Economou <johnlafayeti@gmail.com>
2022-07-21 14:00:24 -0400

    fixed bug where it was sorting by helpful on handling was helpful? this is my last feature/bug fix now its time to really clean up the code

--- src/RatingsReviews/RatingsReviews.jsx
+++ src/RatingsReviews/RatingsReviews.jsx
@@ -3,32 +3,46 @@ import BigList from './BigList.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Main component
-export default function RatingsReviews({metaD, cacheD, trackClick}) {
+export default function RatingsReviews({ metaD, cacheD, trackClick }) {
 
 
   var sortRelevent = function (array) {
-    let currentIndex = array.length,  randomIndex;
-
-    while (currentIndex != 0) {
-
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+    // set a counter = 0
+    var count = 0;
+    // Create a for loop that goes through the entire array
+    for (var i = 0; i < array.length - 1; i++) {
+      // Compare current index value to the neighboring index
+      // if current index is greater than neighboring index
+      console.log('testr', array[i])
+      if (array[i].photos.length < array[i + 1].photos.length) {
+        // swap the values around
+        var currentVal = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = currentVal;
+        // increment a counter
+        count++;
+      }
     }
-
+    // if counter = 0
+    if (count === 0) {
+      // then return the sorted array
+      return array;
+    }
+    // if counter > 0
+    if (count > 0) {
+      // run bubbleSort again with the current array
+      sortRelevent(array);
+    }
     return array;
   }
 
 
   return (
-    <div id = 'reviews' onClick={(e) => trackClick(e, 'Reviews')}>
+    <div id='reviews' onClick={(e) => trackClick(e, 'Reviews')}>
       {console.log('cash', cacheD)}
       <BigList
         metaD={metaD}
         cacheD={sortRelevent(cacheD)} />
     </div>
   );
-
 }
