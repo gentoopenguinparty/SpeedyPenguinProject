18da8700ec88a05013b8fb5268a84b9b0f613704
John Economou <johnlafayeti@gmail.com>
2022-07-22 11:27:59 -0400

    cryomg

--- src/RatingsReviews/AddReview.jsx
+++ src/RatingsReviews/AddReview.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import PhotoCloud from './PhotoCloud.jsx'
 import StarRating from './StarRating.jsx'
 import Size from './Size.jsx'
 import Fit from './Fit.jsx'
