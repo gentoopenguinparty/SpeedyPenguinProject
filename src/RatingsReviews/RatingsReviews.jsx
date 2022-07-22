9114a26d4152f248acdc0eba7ced3eee44e296ab
John Economou <johnlafayeti@gmail.com>
2022-07-22 12:21:14 -0400

    making several changes to get new branch up to date

--- src/RatingsReviews/RatingsReviews.jsx
+++ src/RatingsReviews/RatingsReviews.jsx
@@ -3,14 +3,42 @@ import BigList from './BigList.jsx';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Main component
-export default function RatingsReviews({metaD, cacheD, trackClick}) {
+export default function RatingsReviews({ metaD, cacheD, trackClick }) {
+
+
+  var sortRelevent = function (array) {
+    // set a counter = 0
+    var count = 0;
+    // Create a for loop that goes through the entire array
+    for (var i = 0; i < array.length - 1; i++) {
+      if (array[i].photos.length < array[i + 1].photos.length) {
+        // swap the values around
+        var currentVal = array[i];
+        array[i] = array[i + 1];
+        array[i + 1] = currentVal;
+        // increment a counter
+        count++;
+      }
+    }
+    // if counter = 0
+    if (count === 0) {
+      // then return the sorted array
+      return array;
+    }
+    // if counter > 0
+    if (count > 0) {
+      // run bubbleSort again with the current array
+      sortRelevent(array);
+    }
+    return array;
+  }
+
 
   return (
-    <div id = 'reviews' onClick={(e) => trackClick(e, 'Reviews')}>
+    <div id='reviews' onClick={(e) => trackClick(e, 'Reviews')}>
       <BigList
         metaD={metaD}
-        cacheD={cacheD} />
+        cacheD={sortRelevent(cacheD)} />
     </div>
   );
-
 }
