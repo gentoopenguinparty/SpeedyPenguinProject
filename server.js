285ab64ab6bca1bc2b2a7fa50670cb016a5b583d
Manuel Rosadilla <manurosadilla@gmail.com>
2022-07-22 10:16:41 -0400

    Got server running!

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(port, () => {
